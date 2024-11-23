2b15c546125fc68acd00b124d526f1a1fa6fbf9b
Rodrigo Moldes <moldesrodrigor@gmail.com>
2024-11-23 14:56:37 -0400

    add ssl

--- src/database/database.js
+++ src/database/database.js
@@ -9,6 +9,13 @@ const sequelize = new Sequelize (
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     logging: console.log,
+
+    dialectOption: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
    }
 );
 
